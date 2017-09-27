23025067de24cb3759175da68de4dd41cf3eeeec
Vijay Vadaparthi <31909683+VijayVadaparthi@users.noreply.github.com>
2017-09-27 12:52:48 +0530

    Add Server and LineCook classes

---
+++ LineCook.java
@@ -0,0 +1,8 @@
+package com.rubicon.vjit;
+
+public class LineCook implements RubiconRestaurant {
+	public void prepareFood() {
+		System.out.println(">> Food is prepared by LineCook. Ready to serve");
+	}
+	
+}
---
+++ Server.java
@@ -0,0 +1,9 @@
+package com.rubicon.vjit;
+
+public class Server implements RubiconRestaurant {
+	public void serve() {
+		System.out.println(">> Server collects the food from LineCook"+ "\n"+" Food is served to the Customer by the Server");
+	}
+	
+
+}
