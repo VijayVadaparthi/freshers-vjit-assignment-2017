59c7ee870258d6c48322f1934070f26f522b2d4b
Vijay Vadaparthi <31909683+VijayVadaparthi@users.noreply.github.com>
2017-09-28 22:11:17 +0530

    BusBoy.java class is added

---
+++ BusBoy.java
@@ -0,0 +1,14 @@
+package com.rubicon.vjit;
+
+public class BusBoy {
+	public void serveWater() {
+		System.out.println("\n BusBoy serves Water to the Customer");
+	}
+	public void cleanTable() {
+		System.out.println("\n BusBoy cleans the Table");
+	}
+	public void layoutCloth() {
+		System.out.println("\n BusBoy layouts a new Cloth on the Table");
+	}
+
+}
