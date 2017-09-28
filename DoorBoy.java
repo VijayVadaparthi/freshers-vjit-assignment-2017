b1b4df845e78297ce6f12ac7f067e70bfd06861e
Vijay Vadaparthi <31909683+VijayVadaparthi@users.noreply.github.com>
2017-09-28 22:08:11 +0530

    DoorBoy.java class is added

---
+++ DoorBoy.java
@@ -0,0 +1,11 @@
+package com.rubicon.vjit;
+
+public class DoorBoy {
+	public void greeting1() {
+		System.out.println("\n DOOR BOY:\n Welcome to RubiconRestaurant Sir!\n----------------------------------");
+	}
+	public void greeting2() {
+		System.out.println("\n------------------------------------------\n DOOR BOY:\n Have a Nice day Sir! Please visit again..");
+	}
+
+}
