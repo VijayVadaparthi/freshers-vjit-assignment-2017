66a3230f0d620bb7218f4155f78c708a8a0cfefa
Vijay Vadaparthi <31909683+VijayVadaparthi@users.noreply.github.com>
2017-09-28 22:12:49 +0530

    Exec_Chef.java class is added
    Executive Chef class

---
+++ Exec_Chef.java
@@ -0,0 +1,8 @@
+package com.rubicon.vjit;
+
+public class Exec_Chef {
+	public void receiveOrder() {
+		System.out.println("\n Executive Chef receives order from Manager and expedites the Line Cooks to prepare the food");
+	}
+
+}
