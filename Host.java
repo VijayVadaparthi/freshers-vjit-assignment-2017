40696638ca66dd9cc2322631a6e839c43d15219f
Vijay Vadaparthi <31909683+VijayVadaparthi@users.noreply.github.com>
2017-09-29 22:02:39 +0530

    code added

--- Host.java
+++ Host.java
@@ -3,11 +3,12 @@ package com.rubicon.vjit;
 import java.util.Scanner;
 
 public class Host {
-	public void setTable() {
+	public void setTable() throws InterruptedException {
 		int n;
 		Scanner sc =new Scanner(System.in);
 		System.out.print("\n Enter No. of People to dine : ");
 		n = sc.nextInt();
+		Thread.sleep(1500);
 		System.out.println("\n Host allots Table with "+n+" chairs to the Customer");
 	}
 
